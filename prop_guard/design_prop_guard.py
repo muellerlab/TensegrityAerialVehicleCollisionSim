1. Fix the bug in cross joint stiffness computation 2. Add simulation stop check based on COM velocity direction 3. Update plotting tools for wall-collision simulations.

prop_guard/design_prop_guard.py
@@ -22,9 +22,8 @@ Side:
 [----O---------O----]
 n0  n1        n2   n3
 
-Top: 
-[Case 0: Rotate counterclockwise so that 1,7,3,6 are at the same position as the quadcopter nodes on tensegrity]
-[Case 1: No rotation required]
+Top view: 
+
         '---'8
           O  7
           |
@@ -68,7 +67,6 @@ class prop_guard_design():
             [5,6,2],
             [6,2,7],
             [2,7,8]]
-        
 
         self.crossJoints = [[6,2,1],
                            [3,2,6],
@@ -77,7 +75,7 @@ class prop_guard_design():
 
         self.dRod = self.param.dRod
         self.dJoint = self.param.dJoint
-        self.dCrossJoint = 20*self.dJoint
+        self.dCrossJoint = self.dJoint
         pass
     
     def get_pos_from_comparable_tensegrity(self,propPos):
@@ -124,6 +122,7 @@ class prop_guard_design():
             [b,e] = self.links[i]
             self.linkLength[i] = np.linalg.norm(self.nodePosList[b] - self.nodePosList[e])
             self.kLinkList[i] = self.param.rE*self.rA/(self.linkLength[i])
+        self.baseLengthList = self.linkLength # list of each length components
 
         self.kJointList = np.zeros(len(self.joints))
         self.kJointStressList = np.zeros(len(self.joints)) # Coefficient for max stress at the joint node. 
@@ -134,15 +133,12 @@ class prop_guard_design():
             self.kJointList[i] = jointI*self.param.rE/(jointLength) # moment ~= k*theta, where theta is the bending angle. 
             self.kJointStressList[i] = self.kJointList[i]*self.rR/jointI # stress = Moment*r/I
 
-
         self.kCrossJointList = np.zeros(len(self.crossJoints))
-        self.kCrossJointStressList = np.zeros(len(self.crossJoints)) # Coefficient for max stress at the joint node. 
-
+        self.kCrossJointStressList = np.zeros(len(self.crossJoints)) # Coefficient for max stress at the crossjoint node. 
         for i in range(len(self.crossJoints)):
-            [b,m,e] = self.joints[i]
-            jointLength = np.linalg.norm(self.nodePosList[b] - self.nodePosList[m]) +  np.linalg.norm(self.nodePosList[m] - self.nodePosList[e])
-            jointI = (self.rR**4)*np.pi/4  #Second moment of area 
-            self.kCrossJointList[i] = jointI*self.param.rE/(jointLength) # moment ~= k*theta, where theta is the bending angle. 
-            self.kCrossJointStressList[i] = self.kCrossJointList[i]*self.rR/jointI # stress = Moment*r/I
-
-        self.baseLengthList = self.linkLength # list of each length components
+            [b,m,e] = self.crossJoints[i]
+            crossJointLength = np.linalg.norm(self.nodePosList[b] - self.nodePosList[m]) +  np.linalg.norm(self.nodePosList[m] - self.nodePosList[e])
+            crossJointI = (self.rR**4)*np.pi/4  #Second moment of area 
+            self.kCrossJointList[i] = crossJointI*self.param.rE/(crossJointLength) # moment ~= k*theta, where theta is the bending angle. 
+            self.kCrossJointStressList[i] = self.kCrossJointList[i]*self.rR/crossJointI # stress = Moment*r/I
+        

prop_guard/prop_guard_analysis.py
@@ -20,6 +20,11 @@ class prop_guard_analysis():
         self.kJointList =prop_guard_ode.kJointList
         self.kJointStressList =prop_guard_ode.kJointStressList
         self.dJoint =prop_guard_ode.dJoint
+
+        self.crossJoints = prop_guard_ode.crossJoints
+        self.kCrossJointList = prop_guard_ode.kCrossJointList
+        self.dCrossJoint = prop_guard_ode.dCrossJoint
+        self.kCrossJointStressList = prop_guard_ode.kCrossJointStressList
         pass
 
     def compute_joint_angle_and_torque(self,nodes,vels):
@@ -71,6 +76,54 @@ class prop_guard_analysis():
             jointInfo[jointID, 4] = theta*self.kJointStressList[jointID] # stress due to bending at the joint
         return jointInfo
 
+    def compute_cross_joint_angle_and_torque(self,nodes,vels):
+        # compute joint angle, spring moment and damping moment at the joint
+        crossJointInfo = np.zeros((len(self.crossJoints),5))
+        for cjID in range(len(self.crossJoints)):
+            crossJoint = self.crossJoints[cjID]
+            n0 = nodes[crossJoint[0]]
+            n1 = nodes[crossJoint[1]]
+            n2 = nodes[crossJoint[2]]
+
+            l01 = np.linalg.norm(n1-n0)
+            l12 = np.linalg.norm(n2-n1)
+            e01 = Vec3(n1-n0)/l01
+            e12 = Vec3(n2-n1)/l12
+            
+            cross012 = e01.cross(e12)
+            norm012 = cross012.norm2()
+
+            if norm012 < 1e-10:
+                # If two rods are parallel. No rotation axis can be determined. 
+                # We overwrite it with zero-vector so corresponding torque will be zero
+                rotAxis012 = Vec3(0,0,0) 
+            else:
+                rotAxis012 = cross012/norm012
+            cosTheta = e01.dot(e12)
+            if cosTheta < -(1-1e-12):
+                theta = np.pi
+            elif cosTheta > (1-1e-12):
+                theta = 0
+            else:
+                theta = np.arccos(cosTheta)
+
+            v01 = Vec3(vels[crossJoint[0]] - vels[crossJoint[1]])
+            v0_tan_dir = -rotAxis012.cross(-e01) # when two rods are parallel, this vector is zero. Otherwise, it cross product of two uniform vector perpendicular to each other
+            omega0 = v01.dot(v0_tan_dir)/l01 # angular rate due to rotation of 0-1 rod
+            v21 = Vec3(vels[crossJoint[2]] - vels[crossJoint[1]])
+            v2_tan_dir = rotAxis012.cross(e12) # positive tangential direction for node 2 to increase bending angle
+            omega2 = v21.dot(v2_tan_dir)/l12 # angular rate due to rotation of 1-2 rod
+            
+            M_spring = (theta-np.pi/2)*self.kCrossJointList[cjID]
+            M_damping = (omega0 + omega2)*self.dCrossJoint
+
+            crossJointInfo[cjID, 0] = theta 
+            crossJointInfo[cjID, 1] = omega0+omega2 
+            crossJointInfo[cjID, 2] = M_spring 
+            crossJointInfo[cjID, 3] = M_damping
+            crossJointInfo[cjID, 4] = (theta-np.pi/2)*self.kCrossJointStressList[cjID] # stress due to bending at the joint
+        return crossJointInfo
+
     def compute_link_force(self,nodePos):
         # compute all elastic forces in rods based on node position
         linkForce = np.zeros(len(self.links))
@@ -85,7 +138,7 @@ class prop_guard_analysis():
         # Compute the stress of each link
         return self.compute_link_force(nodePos)/(self.prop_guard_ode.prop_guard.rA)
 
-    def compute_node_max_stress(self,linkStress, jointStress):
+    def compute_node_max_stress(self,linkStress, jointStress, crossJointStress = None):
         # linkStress: len(link) * 1, stress at each link due to compression/extension 
         # jointStress: nodeNum * 1, stress at each node due to bending moment 
         # We compute the max stress each node is under. (Notice that stress on both sides of the node might be different)
@@ -100,9 +153,16 @@ class prop_guard_analysis():
             for jointID in range(len(self.joints)):
                 if nodeID == self.joints[jointID][1]:
                     nodeMaxStress[nodeID] += np.abs(jointStress[jointID])
+        
+        if not (crossJointStress is None):
+            for i in range(len(self.crossJoints)):
+                nodeID = self.crossJoints[i][1]
+                if crossJointStress[i] > nodeMaxStress[nodeID]:
+                    nodeMaxStress[nodeID] = crossJointStress[i] # if cross joint stress is larger, overwrite the node stress with the larger one
+
         return nodeMaxStress
 
-    def compute_joint_forces_for_record(self,nodes, vels, returnStress = False):
+    def compute_joint_forces_for_record(self, nodes, vels, returnStress = False):
         """
         nodes: position vector of nodes 
         vels: velocity vector of nodes
@@ -169,3 +229,14 @@ class prop_guard_analysis():
             return [rotationSpringF, rotationDampingF, jointStress]
         else:
             return [rotationSpringF, rotationDampingF]
+
+    def compute_wall_collision_force(self, nodePos, nWall:Vec3, kWall, pWall:Vec3):
+        # Compute the stress of each link
+        nodeF = np.zeros(self.nodeNum)
+        nWall = nWall/nWall.norm2() #normalize the direction vector
+        for i in range(self.nodeNum):
+            d=(Vec3(nodePos[i])-pWall).dot(nWall)
+            if d<0:
+                normalForce = -kWall*d*nWall
+                nodeF[i] = normalForce.norm2()
+        return nodeF

prop_guard/prop_guard_ode.py
@@ -19,8 +19,8 @@ class prop_guard_ode():
         self.rL = prop_guard.rL
 
         self.joints = prop_guard.joints
-        self.kJointList =prop_guard.kJointList
-        self.dJoint =prop_guard.dJoint
+        self.kJointList = prop_guard.kJointList
+        self.dJoint = prop_guard.dJoint
         self.kJointStressList = prop_guard.kJointStressList
 
         self.crossJoints = prop_guard.crossJoints
@@ -76,7 +76,7 @@ class prop_guard_ode():
             theta = np.arccos(cosTheta)
 
         if crossJointFlag:
-            theta = theta - np.pi/2
+            theta = theta - np.pi/2 #default angle for cross joint is pi/2
 
         v01 = Vec3(vels[joint[0]] - vels[joint[1]])
         v0_tan_dir = -rotAxis012.cross(-e01) # when two rods are parallel, this vector is zero. Otherwise, it cross product of two uniform vector perpendicular to each other
@@ -127,7 +127,6 @@ class prop_guard_ode():
             rotF[self.joints[jointID][1]] += F1
             rotF[self.joints[jointID][2]] += F2
         
-
         for jointID in range(len(self.crossJoints)):
             [F0, F1, F2] =  self.compute_joint_forces(nodes,vels,jointID,True)
             rotF[self.crossJoints[jointID][0]] += F0
@@ -140,6 +139,7 @@ class prop_guard_ode():
             return springF + dampF + rotF
 
     def ode_ivp_hit_force(self,t, P, extF, ft):
+        # Simulate with constant external force described in world frame
         dPdt = np.zeros_like(P)
         dPdt[:self.nodeNum*self.dim] = P[self.nodeNum*self.dim:]
         forces = self.compute_internal_forces(P)
@@ -150,15 +150,8 @@ class prop_guard_ode():
             dPdt[self.nodeNum*self.dim+self.dim*i:self.nodeNum*self.dim+self.dim*(i+1)] = forces[i]/self.massList[i]
         return dPdt 
 
-    def ode_ivp(self,t, P):
-        dPdt = np.zeros_like(P)
-        dPdt[:self.nodeNum*self.dim] = P[self.nodeNum*self.dim:]
-        forces = self.compute_internal_forces(P)
-        for i in range(self.nodeNum):
-            dPdt[self.nodeNum*self.dim+self.dim*i:self.nodeNum*self.dim+self.dim*(i+1)] = forces[i]/self.massList[i]
-        return dPdt 
-
     def ode_ivp_force_body_frame(self, t, P, totalF, rot:Rotation):
+        # Simulate with constant external force described in body frame
         dPdt = np.zeros_like(P)
         dPdt[:self.nodeNum*self.dim] = P[self.nodeNum*self.dim:]
         forces = self.compute_internal_forces(P)
@@ -175,6 +168,15 @@ class prop_guard_ode():
             dPdt[self.nodeNum*self.dim+self.dim*i:self.nodeNum*self.dim+self.dim*(i+1)] = forces[i]/self.massList[i]
         return dPdt 
 
+    def ode_ivp(self,t, P):
+        # Simulate with no external forces 
+        dPdt = np.zeros_like(P)
+        dPdt[:self.nodeNum*self.dim] = P[self.nodeNum*self.dim:]
+        forces = self.compute_internal_forces(P)
+        for i in range(self.nodeNum):
+            dPdt[self.nodeNum*self.dim+self.dim*i:self.nodeNum*self.dim+self.dim*(i+1)] = forces[i]/self.massList[i]
+        return dPdt 
+
     def ode_ivp_wall(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
         """
         nWall: a normal vector pointing out of the wal
@@ -182,7 +184,6 @@ class prop_guard_ode():
         kWall: Hooke's constant of the wall
         wallX: x coordinate of the wall surface. Any point with pos.x < wallX is in the wall
         kWall: Hooke's coefficient of the wall 
-
         Assume the wall is a very stiff spring.
         """
         dPdt = np.zeros_like(P)
@@ -200,7 +201,6 @@ class prop_guard_ode():
             dPdt[self.nodeNum*self.dim+self.dim*i:self.nodeNum*self.dim+self.dim*(i+1)] = forces[i]/self.massList[i]
         return dPdt 
 
-
     def eventAttr():
         def decorator(func):
             func.direction = 1
@@ -214,6 +214,8 @@ class prop_guard_ode():
         Check if the structure has stopped touching the wall. 
         To decrease the computation. We assume: 1) normal direction of the wall is +x direction
                                                 2) the wall surface is at the plane that x=0
+        This function can be used to help terminate the simulation once the vehicle is away from the wall 
+
         """
         nodes = P[:self.nodeNum*self.dim].reshape((self.nodeNum,self.dim))
         dMin = np.min(nodes[:,0])        
@@ -225,10 +227,22 @@ class prop_guard_ode():
         Check if the structure has stopped touching the wall. 
         Here we compute the smallest distance from structure to the wall
         """
+        dGap = self.rL/20 #gap distance to terminate the simualtion
         dMin = -self.rL
         nodes = P[:self.nodeNum*self.dim].reshape((self.nodeNum,self.dim))  
         for i in range(self.nodeNum):
             d=(Vec3(nodes[i])-pWall).dot(nWall)
             if d>dMin:
                 dMin = d
-        return dMin-self.rL/20
+        return (dMin-dGap)
+    
+    @eventAttr()
+    def vel_check_simple(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
+        """
+        Stop the simulation when COM velocity is pointing away from the wall with a certain threshold speed.
+        We expect maximum deformation to take place before this moment.
+        """
+        threshold = 0.1 #[m/s]
+        vels = P[self.nodeNum*self.dim:].reshape((self.nodeNum,self.dim)) 
+        vel_x = vels[:,0]
+        return (vel_x-threshold).dot(self.massList) #weighted average of node velocity

simulation_prop_guard_wall_collision.py
@@ -1,3 +1,4 @@
+from platform import node
 import numpy as np
 import scipy as sp
 from py3dmath.py3dmath import Rotation, Vec3
@@ -13,13 +14,13 @@ import seaborn as sns
 sns.set_theme()
 
 """
-Simulate the dynamics of a single tensegrity under a sudden external load.
-Plot the forces in the system and animate the process
-Type of simulation: 
-"InitialMomentum" adds a momentum to the end nodes of the structure
-"ConstantForceWorldFrame" exerts constant world frame forces at the end nodes 
-"ConstantForceBodyFrame" exerts constant body frame external force at the end nodes
+Simulate the dynamics of a propeller guard vehicle under wall collision. 
+Plot the information in the system and animate the process
 """
+# Setup running functions
+drawDebugPlots = True
+createAnimation = True
+
 # Create the prop-guard design
 param = design_param()
 prop_guard = prop_guard_design(param)
@@ -29,24 +30,23 @@ nodeNum = prop_guard.nodeNum
 dim = prop_guard.dim
 joints = prop_guard.joints
 links = prop_guard.links
+crossJoints = prop_guard.crossJoints
 massList = prop_guard.massList
 
 # Setup simulation experiment
 t0 = 0 # [s]
-tf = 0.02 # [s] Simulation time
+tf = 0.05 # [s] Simulation time
 t_span = (t0,tf)
 speed = 5 # speed of collision
 
-# Rotate the vehicle to default setup: two propellers facing the wall
-
+# Rotate the vehicle to default orientation: two propeller guards 
 propRot = Rotation.from_euler_YPR([np.pi/4,0,0])
 defaultPos = np.zeros_like(prop_guard.nodePosList)
 for i in range(nodeNum):
     defaultPos[i] = (propRot*Vec3(prop_guard.nodePosList[i])).to_array().squeeze()
 
 # Rotate the vehicle to desired attitude
-# att = Rotation.from_euler_YPR([np.pi/4,-np.pi/4,0])
-att = Rotation.from_euler_YPR([0,0,0])
+att = Rotation.from_euler_YPR([0,-np.pi/4,0])
 initPos = np.zeros_like(prop_guard.nodePosList)
 for i in range(nodeNum):
     initPos[i] = (att*Vec3(defaultPos[i])).to_array().squeeze()
@@ -72,7 +72,7 @@ kWall = Ew*Aw/Lw #[N/m] Stiffness of wall
 
 print("Simulation type: wall collision")
 prop_guard_ODE =prop_guard_ode(prop_guard)
-sol = solve_ivp(prop_guard_ODE.ode_ivp_wall, t_span, P0, method='Radau',args=(nWall, kWall, pWall), events=prop_guard_ODE.wall_check_simple)
+sol = solve_ivp(prop_guard_ODE.ode_ivp_wall, t_span, P0, method='Radau',args=(nWall, kWall, pWall), events=prop_guard_ODE.vel_check_simple)
 print("Finish Simulation")
 
 # Analyze the ODE result
@@ -87,9 +87,11 @@ jointAngleHist = np.zeros((stepCount,len(joints)))
 jointAngularRateHist = np.zeros((stepCount,len(joints)))
 jointSpringMomentHist = np.zeros((stepCount,len(joints)))
 jointDampingMomentHist = np.zeros((stepCount,len(joints)))
-jointStressHist = np.zeros((stepCount,len(joints)))
 linkStressHist = np.zeros((stepCount,len(links)))
+jointStressHist = np.zeros((stepCount,len(joints)))
+crossJointStressHist = np.zeros((stepCount,len(crossJoints)))
 nodeMaxStressHist = np.zeros((stepCount,nodeNum))
+extForceHist = np.zeros((stepCount,nodeNum)) # external force for node collision
 
 for i in range(nodeNum):
     for j in range(stepCount):
@@ -97,125 +99,139 @@ for i in range(nodeNum):
         nodeVelHist[j,i,:] = Ps[nodeNum*dim+i*dim:nodeNum*dim+(i+1)*dim,j]
 for i in range(stepCount):
     jointInfo_i = prop_guard_helper.compute_joint_angle_and_torque(nodePosHist[i],nodeVelHist[i])
+    crossJointInfo_i = prop_guard_helper.compute_cross_joint_angle_and_torque(nodePosHist[i],nodeVelHist[i])
     jointAngleHist[i,:] = jointInfo_i[:,0]
     jointAngularRateHist[i,:] = jointInfo_i[:,1]
     jointSpringMomentHist[i,:] = jointInfo_i[:,2]
     jointDampingMomentHist[i,:] = jointInfo_i[:,3]
     jointStressHist[i,:] = jointInfo_i[:,4]
+    crossJointStressHist[i,:] =crossJointInfo_i[:,4]
     linkStressHist[i,:] = prop_guard_helper.compute_link_stress(nodePosHist[i])
-    nodeMaxStressHist[i,:] = prop_guard_helper.compute_node_max_stress(linkStressHist[i], jointStressHist[i,:])
+    nodeMaxStressHist[i,:] = prop_guard_helper.compute_node_max_stress(linkStressHist[i], jointStressHist[i,:],crossJointStressHist[i,:])
+    extForceHist[i,:] = prop_guard_helper.compute_wall_collision_force(nodePosHist[i],nWall, kWall, pWall)
 
-# Plot used in paper 
-figp = plt.figure(figsize=(36,7))
-n = 1 # num sub-plots
-figp.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    figp.add_subplot(n, 1, i, sharex=figp.axes[0])
+if drawDebugPlots:
+    # Debug plots 
+    fig = plt.figure()
+    n = 1 # num sub-plots
+    fig.add_subplot(n, 1, 1)
 
-figp.axes[0].plot(tHist, nodeMaxStressHist[:,joints[0][1]], 'r-', label='Joint0&3', linewidth=6)
-figp.axes[0].plot(tHist, nodeMaxStressHist[:,joints[1][1]], 'b-', label='Joint1&4', linewidth=6)
-figp.axes[0].plot(tHist, nodeMaxStressHist[:,joints[2][1]], 'y-', label='Joint2', linewidth=6)
-figp.axes[0].set_ylabel('Stress in the propeller guard frame [Pa]', fontsize=25)
-figp.axes[0].set_xlabel('Time [s]', fontsize=25)
-figp.axes[0].legend(fontsize=20)
-figp.axes[0].tick_params(axis='x', labelsize=20)
-figp.axes[0].tick_params(axis='y', labelsize=20)
-figp.axes[0].yaxis.get_offset_text().set_fontsize(20)
+    for i in range(2, n + 1):
+        fig.add_subplot(n, 1, i, sharex=fig.axes[0])
 
-# Debug plots 
-fig = plt.figure()
-n = 1 # num sub-plots
-fig.add_subplot(n, 1, 1)
+    for j in range(len(links)):
+        fig.axes[0].plot(tHist, linkStressHist[:,j], '-', label='Rod_'+str(j))
+    fig.axes[0].set_ylabel('Stress [Pa]')
+    fig.axes[0].set_xlabel('Time [s]')
+    fig.axes[0].set_title('Stress in rods')
+    fig.axes[0].legend()
 
-for i in range(2, n + 1):
-    fig.add_subplot(n, 1, i, sharex=fig.axes[0])
+    fig2 = plt.figure()
+    n = 2 # num sub-plots
+    fig2.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig2.add_subplot(n, 1, i, sharex=fig2.axes[0])
+    for j in range(len(joints)):
+        fig2.axes[0].plot(tHist, jointSpringMomentHist[:,j], '-', label='bendingMomentJoint_'+str(j))
+        fig2.axes[1].plot(tHist, jointDampingMomentHist[:,j], '-', label='dampingMomentJoint_'+str(j))
+    fig2.axes[0].set_ylabel('Moment [Nm]')
+    fig2.axes[1].set_ylabel('Moment [Nm]')
+    fig2.axes[0].set_xlabel('Time [s]')
+    fig2.axes[0].legend()
+    fig2.axes[1].legend()
+    fig2.axes[0].set_title('Joint spring moment')
+    fig2.axes[1].set_title('Joint damping moment')
 
-for j in range(len(links)):
-    fig.axes[0].plot(tHist, linkStressHist[:,j], '-', label='Link_'+str(j))
-fig.axes[0].set_ylabel('Stress [Pa]')
-fig.axes[0].set_xlabel('Time [s]')
-fig.axes[0].legend()
+    fig3 = plt.figure()
+    n = nodeNum # num sub-plots
+    fig3.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig3.add_subplot(n, 1, i, sharex=fig3.axes[0])
 
-fig2 = plt.figure()
-n = 2 # num sub-plots
-fig2.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig2.add_subplot(n, 1, i, sharex=fig2.axes[0])
-for j in range(len(joints)):
-    fig2.axes[0].plot(tHist, jointSpringMomentHist[:,j], '-', label='bendingMomentJoint_'+str(j))
-    fig2.axes[1].plot(tHist, jointDampingMomentHist[:,j], '-', label='dampingMomentJoint_'+str(j))
-fig2.axes[0].set_ylabel('Moment [Nm]')
-fig2.axes[1].set_ylabel('Moment [Nm]')
-fig2.axes[0].set_xlabel('Time [s]')
-fig2.axes[0].legend()
-fig2.axes[1].legend()
+    for j in range(nodeNum):
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,0], 'r-', label='pos'+str(j)+'x')
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,1], 'g-', label='pos'+str(j)+'y')
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,2], 'b-', label='pos'+str(j)+'z')
+        fig3.axes[j].set_ylabel('Pos [m]')
+        fig3.axes[j].set_xlabel('Time [s]')
+        fig3.axes[j].legend()
+    fig3.axes[0].set_title('Node position')
 
-fig3 = plt.figure()
-n = nodeNum # num sub-plots
-fig3.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig3.add_subplot(n, 1, i, sharex=fig3.axes[0])
+    fig4 = plt.figure()
+    n = nodeNum # num sub-plots
+    fig4.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig4.add_subplot(n, 1, i, sharex=fig4.axes[0])
 
-for j in range(nodeNum):
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,0], 'r-', label='pos'+str(j)+'x')
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,1], 'g-', label='pos'+str(j)+'y')
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,2], 'b-', label='pos'+str(j)+'z')
-    fig3.axes[j].set_ylabel('Pos [m]')
-    fig3.axes[j].set_xlabel('Time [s]')
-    fig3.axes[j].legend()
-
-fig4 = plt.figure()
-n = nodeNum # num sub-plots
-fig4.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig4.add_subplot(n, 1, i, sharex=fig4.axes[0])
-
-for j in range(nodeNum):
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,0], 'r-', label='vel'+str(j)+'x')
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,1], 'g-', label='vel'+str(j)+'y')
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,2], 'b-', label='vel'+str(j)+'z')
-    fig4.axes[j].set_ylabel('Vel [m/s]')
-    fig4.axes[j].set_xlabel('Time [s]')
-    fig4.axes[j].legend()
-
-fig5 = plt.figure()
-n = 2 # num sub-plots
-fig5.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig5.add_subplot(n, 1, i, sharex=fig5.axes[0])
-for j in range(len(joints)):
-    fig5.axes[0].plot(tHist, jointAngleHist[:,j], '-', label='angleJoint'+str(j))
-    fig5.axes[1].plot(tHist, jointAngularRateHist[:,j], '-', label='omegaJoint'+str(j))
-fig5.axes[0].set_ylabel('Angle [Rad]')
-fig5.axes[1].set_ylabel('Angular Rate [Rad/s]')
-fig5.axes[0].set_xlabel('Time [s]')
-fig5.axes[0].legend()
-fig5.axes[1].legend()
-
-fig6 = plt.figure()
-n = 2 # num sub-plots
-fig6.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig6.add_subplot(n, 1, i, sharex=fig5.axes[0])
-for j in range(len(joints)):
-    fig6.axes[0].plot(tHist, jointStressHist[:,j], '-', label='stressJoint'+str(j))
-
-for j in range(nodeNum):
-    fig6.axes[1].plot(tHist, nodeMaxStressHist[:,j], '-', label='maxStressNode'+str(j))
-fig6.axes[0].set_ylabel('Stress [Pa]')
-fig6.axes[1].set_ylabel('Stress [Pa]')
-fig6.axes[0].set_xlabel('Time [s]')
-fig6.axes[0].legend()
-fig6.axes[1].legend()
+    for j in range(nodeNum):
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,0], 'r-', label='vel'+str(j)+'x')
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,1], 'g-', label='vel'+str(j)+'y')
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,2], 'b-', label='vel'+str(j)+'z')
+        fig4.axes[j].set_ylabel('Vel [m/s]')
+        fig4.axes[j].set_xlabel('Time [s]')
+        fig4.axes[j].legend()
+    fig4.axes[0].set_title('Node velocity')
 
 
-frameSampleRate = 100 # use 1 frame for each 100 samples 
-animateIteration = stepCount//frameSampleRate
-nodePosAnimateData = np.zeros((animateIteration,nodeNum,dim))
-for i in range(animateIteration):
-    nodePosAnimateData[i] = nodePosHist[i*frameSampleRate]
+    fig5 = plt.figure()
+    n = 2 # num sub-plots
+    fig5.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig5.add_subplot(n, 1, i, sharex=fig5.axes[0])
+    for j in range(len(joints)):
+        fig5.axes[0].plot(tHist, jointAngleHist[:,j], '-', label='angleJoint'+str(j))
+        fig5.axes[1].plot(tHist, jointAngularRateHist[:,j], '-', label='omegaJoint'+str(j))
+    fig5.axes[0].set_ylabel('Angle [Rad]')
+    fig5.axes[1].set_ylabel('Angular Rate [Rad/s]')
+    fig5.axes[0].set_xlabel('Time [s]')
+    fig5.axes[0].legend()
+    fig5.axes[1].legend()
+    fig5.axes[0].set_title('Joint angle')
+    fig5.axes[1].set_title('Joint angular velocity')
 
-print("Creating Animation")
-animator = prop_guard_animator(prop_guard)
-animator.plot_wall(pWall,'x')
-animator.animate_prop_guard(nodePosHist,True,False)
+    fig6 = plt.figure()
+    n = 2 # num sub-plots
+    fig6.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig6.add_subplot(n, 1, i, sharex=fig6.axes[0])
+    for j in range(len(joints)):
+        fig6.axes[0].plot(tHist, jointStressHist[:,j], '-', label='stressJoint'+str(j))
+    for j in range(len(crossJoints)):
+        fig6.axes[0].plot(tHist, crossJointStressHist[:,j], '-', label='stressCrossJoint'+str(j))
+    for j in range(nodeNum):
+        fig6.axes[1].plot(tHist, nodeMaxStressHist[:,j], '-', label='maxStressNode'+str(j))
+    fig6.axes[0].set_ylabel('Stress [Pa]')
+    fig6.axes[1].set_ylabel('Stress [Pa]')
+    fig6.axes[0].set_xlabel('Time [s]')
+    fig6.axes[0].legend()
+    fig6.axes[1].legend()
+    fig6.axes[0].set_title('Stress in joint')
+    fig6.axes[1].set_title('Max stress in node')
+
+    # Fig7, external collison force from wall
+    fig7 = plt.figure()
+    n = 1 # num sub-plots
+    fig7.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig7.add_subplot(n, 1, i, sharex=fig7.axes[0])
+    for j in range(nodeNum):
+        fig7.axes[0].plot(tHist, extForceHist[:,j], '-', label='extF'+str(j))
+    fig7.axes[0].set_ylabel('Force [N]')
+    fig7.axes[0].set_xlabel('Time [s]')
+    fig7.axes[0].legend()
+    fig7.axes[0].set_title('Wall collision force')
+
+    if not(createAnimation):
+        plt.show()
+
+if createAnimation:
+    frameSampleRate = 100 # use 1 frame for each 100 samples 
+    animateIteration = stepCount//frameSampleRate
+    nodePosAnimateData = np.zeros((animateIteration,nodeNum,dim))
+    for i in range(animateIteration):
+        nodePosAnimateData[i] = nodePosHist[i*frameSampleRate]
+
+    print("Creating Animation")
+    animator = prop_guard_animator(prop_guard)
+    animator.plot_wall(pWall,'x')
+    animator.animate_prop_guard(nodePosHist,True,False)

simulation_tensegrity_wall_collision.py
@@ -10,6 +10,16 @@ from scipy.integrate import solve_ivp
 import seaborn as sns
 sns.set_theme()
 
+"""
+Simulate the dynamics of a tensegrity under wall collision. 
+Plot the information in the system and animate the process
+"""
+
+# Setup running functions
+drawDebugPlots = True
+createAnimation = True
+
+
 # Setup tensegrity
 param = design_param() #Load structure & material parameter 
 tensegrity = tensegrity_design(param)
@@ -33,7 +43,7 @@ massList = tensegrity.massList
 
 # Setup simulation experiment
 t0 = 0 # [s]
-tf = 0.05
+tf = 0.02
 # [s] Simulation time
 t_span = (t0,tf)
 speed = 5 # speed of collision
@@ -46,8 +56,7 @@ for i in range(nodeNum):
     defaultPos[i] = (tensegrityRot*Vec3(tensegrity.nodePos[i])).to_array().squeeze()
 
 # Rotate the vehicle to desired attitude
-# att = Rotation.from_euler_YPR([np.pi/2,-np.pi/2,0])
-att = Rotation.from_euler_YPR([0,0,0])
+att = Rotation.from_euler_YPR([0,-np.pi/4,0])
 initPos = np.zeros_like(tensegrity.nodePos)
 for i in range(nodeNum):
     initPos[i] = (att*Vec3(defaultPos[i])).to_array().squeeze()
@@ -74,7 +83,7 @@ kWall = Ew*Aw/Lw #[N/m] Stiffness of wall
 print("Simulation type: wall collision")
 print("Begin Simulation")
 tensegrity_ODE =tensegrity_ode(tensegrity)
-sol = solve_ivp(tensegrity_ODE.ode_ivp_wall, t_span, P0, method='Radau',args=(nWall, kWall, pWall),events=tensegrity_ODE.wall_check_simple)
+sol = solve_ivp(tensegrity_ODE.ode_ivp_wall, t_span, P0, method='Radau',args=(nWall, kWall, pWall), events=tensegrity_ODE.vel_check_simple)
 print("Finish Simulation")
 # Analyze the ODE result
 print("Recording Results")
@@ -99,6 +108,7 @@ jointDampingMomentHist = np.zeros((stepCount,len(joints)))
 jointStressHist = np.zeros((stepCount,len(joints)))
 nodeMaxStressHist = np.zeros((stepCount,nodeNum))
 minDistPropToSurface = np.zeros((stepCount,param.propNum))
+extForceHist = np.zeros((stepCount,nodeNum)) # external force for node collision
 
 for i in range(nodeNum):
     for j in range(stepCount):
@@ -121,120 +131,153 @@ for i in range(stepCount):
     jointStressHist[i,:] = jointInfo_i[:,4]
     nodeMaxStressHist[i,:] = tensegrity_helper.compute_node_max_stress(rodStressHist[i], jointStressHist[i,:])
     minDistPropToSurface[i,:] = tensegrity_helper.compute_min_prop_dist_to_face(nodePosHist[i],propOffSet)
+    extForceHist[i,:] = tensegrity_helper.compute_wall_collision_force(nodePosHist[i],nWall, kWall, pWall)
 
-# Fig used for debug
-fig = plt.figure()
-n = 2 # num sub-plots
-fig.add_subplot(n, 1, 1)
+if drawDebugPlots:
 
-for i in range(2, n + 1):
-    fig.add_subplot(n, 1, i, sharex=fig.axes[0])
+    # Fig used for debug
+    fig = plt.figure()
+    n = 2 # num sub-plots
+    fig.add_subplot(n, 1, 1)
 
-for j in range(24):
-    fig.axes[0].plot(tHist, stringForceHist[:,j], '-', label='fString_'+str(j))
-    fig.axes[0].set_ylabel('Force [N]')
-    fig.axes[0].set_xlabel('Time [s]')
-    fig.axes[0].legend()
+    for i in range(2, n + 1):
+        fig.add_subplot(n, 1, i, sharex=fig.axes[0])
 
-for j in range(6):
-    fig.axes[1].plot(tHist, rodForceHist[:,j], '-', label='fRod_'+str(j))
-    fig.axes[1].set_ylabel('Force [N]')
-    fig.axes[1].set_xlabel('Time [s]')
-    fig.axes[1].legend()
+    for j in range(24):
+        fig.axes[0].plot(tHist, stringForceHist[:,j], '-', label='fString_'+str(j))
+        fig.axes[0].set_ylabel('Force [N]')
+        fig.axes[0].set_xlabel('Time [s]')
+        fig.axes[0].legend()
 
-fig2 = plt.figure()
-n = 2 # num sub-plots
-fig2.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig2.add_subplot(n, 1, i, sharex=fig2.axes[0])
+    for j in range(6):
+        fig.axes[1].plot(tHist, rodForceHist[:,j], '-', label='fRod_'+str(j))
+        fig.axes[1].set_ylabel('Force [N]')
+        fig.axes[1].set_xlabel('Time [s]')
+        fig.axes[1].legend()
+    fig.axes[0].set_title('String force')
+    fig.axes[1].set_title('Rod force')
 
-for j in range(len(joints)):
-    fig2.axes[0].plot(tHist, jointSpringMomentHist[:,j], '-', label='Joint '+str(j), linewidth=3)
-    fig2.axes[1].plot(tHist, jointDampingMomentHist[:,j], '-', label='dampingMomentJoint_'+str(j))
-fig2.axes[0].set_ylabel('Moment [Nm]')
-fig2.axes[1].set_ylabel('Moment [Nm]')
-fig2.axes[1].set_xlabel('Time [s]')
-fig2.axes[0].legend()
-fig2.axes[1].legend()
+    fig2 = plt.figure()
+    n = 2 # num sub-plots
+    fig2.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig2.add_subplot(n, 1, i, sharex=fig2.axes[0])
 
-fig3 = plt.figure()
-n = nodeNum # num sub-plots
-fig3.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig3.add_subplot(n, 1, i, sharex=fig3.axes[0])
-
-for j in range(nodeNum):
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,0], 'r-', label='pos'+str(j)+'x')
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,1], 'g-', label='pos'+str(j)+'y')
-    fig3.axes[j].plot(tHist, nodePosHist[:,j,2], 'b-', label='pos'+str(j)+'z')
-    fig3.axes[j].set_ylabel('Pos [m]')
-    fig3.axes[j].set_xlabel('Time [s]')
-    fig3.axes[j].legend()
-
-fig4 = plt.figure()
-n = nodeNum # num sub-plots
-fig4.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig4.add_subplot(n, 1, i, sharex=fig4.axes[0])
-
-for j in range(nodeNum):
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,0], 'r-', label='vel'+str(j)+'x')
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,1], 'g-', label='vel'+str(j)+'y')
-    fig4.axes[j].plot(tHist, nodeVelHist[:,j,2], 'b-', label='vel'+str(j)+'z')
-    fig4.axes[j].set_ylabel('Vel [m/s]')
-    fig4.axes[j].set_xlabel('Time [s]')
-    fig4.axes[j].legend()
-
-fig5 = plt.figure()
-n = 2 # num sub-plots
-fig5.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig5.add_subplot(n, 1, i, sharex=fig5.axes[0])
-for j in range(len(joints)):
-    fig5.axes[0].plot(tHist, jointAngleHist[:,j], '-', label='angleJoint'+str(j))
-    fig5.axes[1].plot(tHist, jointAngularRateHist[:,j], '-', label='omegaJoint'+str(j))
-fig5.axes[0].set_ylabel('Angle [Rad]')
-fig5.axes[1].set_ylabel('Angular Rate [Rad/s]')
-fig5.axes[0].set_xlabel('Time [s]')
-fig5.axes[0].legend()
-fig5.axes[1].legend()
-
-fig6 = plt.figure()
-n = 2 # num sub-plots
-fig6.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig6.add_subplot(n, 1, i, sharex=fig5.axes[0])
-for j in range(len(joints)):
-    fig6.axes[0].plot(tHist, jointStressHist[:,j], '-', label='stressJoint'+str(j))
-for j in range(nodeNum):
-    fig6.axes[1].plot(tHist, nodeMaxStressHist[:,j], '-', label='maxStressNode'+str(j))
-fig6.axes[0].set_ylabel('Stress [Pa]')
-fig6.axes[1].set_ylabel('Stress [Pa]')
-fig6.axes[0].set_xlabel('Time [s]')
-fig6.axes[0].legend()
-fig6.axes[1].legend()
+    for j in range(len(joints)):
+        fig2.axes[0].plot(tHist, jointSpringMomentHist[:,j], '-', label='Joint '+str(j), linewidth=3)
+        fig2.axes[1].plot(tHist, jointDampingMomentHist[:,j], '-', label='dampingMomentJoint_'+str(j))
+    fig2.axes[0].set_ylabel('Moment [Nm]')
+    fig2.axes[1].set_ylabel('Moment [Nm]')
+    fig2.axes[1].set_xlabel('Time [s]')
+    fig2.axes[0].legend()
+    fig2.axes[1].legend()
+    fig2.axes[0].set_title('Joint spring moment')
+    fig2.axes[1].set_title('Joint damping moment')
 
 
-fig7 = plt.figure()
-n = 1 # num sub-plots
-fig7.add_subplot(n, 1, 1)
-for i in range(2, n + 1):
-    fig7.add_subplot(n, 1, i, sharex=fig4.axes[0])
+    fig3 = plt.figure()
+    n = nodeNum # num sub-plots
+    fig3.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig3.add_subplot(n, 1, i, sharex=fig3.axes[0])
 
-for j in range(param.propNum):
-    fig7.axes[0].plot(tHist, minDistPropToSurface[:,j], 'rgbc'[j]+'-', label='minDist'+str(j))
-fig7.axes[0].set_ylabel('DistToSurface [m/s]')
-fig7.axes[0].set_xlabel('Time [s]')
-fig7.axes[0].legend()
+    for j in range(nodeNum):
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,0], 'r-', label='pos'+str(j)+'x')
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,1], 'g-', label='pos'+str(j)+'y')
+        fig3.axes[j].plot(tHist, nodePosHist[:,j,2], 'b-', label='pos'+str(j)+'z')
+        fig3.axes[j].set_ylabel('Pos [m]')
+        fig3.axes[j].set_xlabel('Time [s]')
+        fig3.axes[j].legend()
+    fig3.axes[0].set_title('Node position')
 
-# Animation
-frameSampleRate = 10 #use 1 frame for each 1 sample 
-animateIteration = stepCount//frameSampleRate
-nodePosAnimateData = np.zeros((animateIteration,nodeNum,dim))
-for i in range(animateIteration):
-    nodePosAnimateData[i] = nodePosHist[i*frameSampleRate]
+    fig4 = plt.figure()
+    n = nodeNum # num sub-plots
+    fig4.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig4.add_subplot(n, 1, i, sharex=fig4.axes[0])
 
-print("Creating Animation")
-animator = tensegrity_animator(tensegrity)
-animator.plot_wall(pWall,'x')
-animator.animate_tensegrity(nodePosHist,True,False)
+    for j in range(nodeNum):
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,0], 'r-', label='vel'+str(j)+'x')
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,1], 'g-', label='vel'+str(j)+'y')
+        fig4.axes[j].plot(tHist, nodeVelHist[:,j,2], 'b-', label='vel'+str(j)+'z')
+        fig4.axes[j].set_ylabel('Vel [m/s]')
+        fig4.axes[j].set_xlabel('Time [s]')
+        fig4.axes[j].legend()
+    fig4.axes[0].set_title('Node velocity')
+
+    fig5 = plt.figure()
+    n = 2 # num sub-plots
+    fig5.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig5.add_subplot(n, 1, i, sharex=fig5.axes[0])
+    for j in range(len(joints)):
+        fig5.axes[0].plot(tHist, jointAngleHist[:,j], '-', label='angleJoint'+str(j))
+        fig5.axes[1].plot(tHist, jointAngularRateHist[:,j], '-', label='omegaJoint'+str(j))
+    fig5.axes[0].set_ylabel('Angle [Rad]')
+    fig5.axes[1].set_ylabel('Angular Rate [Rad/s]')
+    fig5.axes[0].set_xlabel('Time [s]')
+    fig5.axes[0].legend()
+    fig5.axes[1].legend()
+    fig5.axes[0].set_title('Joint angle')
+    fig5.axes[0].set_title('Joint angular velocity')
+
+
+    fig6 = plt.figure()
+    n = 2 # num sub-plots
+    fig6.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig6.add_subplot(n, 1, i, sharex=fig5.axes[0])
+    for j in range(len(joints)):
+        fig6.axes[0].plot(tHist, jointStressHist[:,j], '-', label='stressJoint'+str(j))
+    for j in range(nodeNum):
+        fig6.axes[1].plot(tHist, nodeMaxStressHist[:,j], '-', label='maxStressNode'+str(j))
+    fig6.axes[0].set_ylabel('Stress [Pa]')
+    fig6.axes[1].set_ylabel('Stress [Pa]')
+    fig6.axes[0].set_xlabel('Time [s]')
+    fig6.axes[0].legend()
+    fig6.axes[1].legend()
+    fig6.axes[0].set_title('Stress in joint')
+    fig6.axes[1].set_title('Max stress in node')
+
+
+    fig7 = plt.figure()
+    n = 1 # num sub-plots
+    fig7.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig7.add_subplot(n, 1, i, sharex=fig4.axes[0])
+
+    for j in range(param.propNum):
+        fig7.axes[0].plot(tHist, minDistPropToSurface[:,j], 'rgbc'[j]+'-', label='minDist'+str(j))
+    fig7.axes[0].set_ylabel('DistToSurface [m/s]')
+    fig7.axes[0].set_xlabel('Time [s]')
+    fig7.axes[0].legend()
+    fig7.axes[0].set_title('Propeller distance to surface')
+
+    # Fig8, external collison force from wall
+    fig8 = plt.figure()
+    n = 1 # num sub-plots
+    fig8.add_subplot(n, 1, 1)
+    for i in range(2, n + 1):
+        fig8.add_subplot(n, 1, i, sharex=fig8.axes[0])
+    for j in range(nodeNum):
+        fig8.axes[0].plot(tHist, extForceHist[:,j], '-', label='extF'+str(j))
+    fig8.axes[0].set_ylabel('Force [N]')
+    fig8.axes[0].set_xlabel('Time [s]')
+    fig8.axes[0].legend()
+    fig8.axes[0].set_title('Wall collision force')
+
+    if not(createAnimation):
+        plt.show()
+
+if createAnimation:
+    # Animation
+    frameSampleRate = 10 #use 1 frame for each 1 sample 
+    animateIteration = stepCount//frameSampleRate
+    nodePosAnimateData = np.zeros((animateIteration,nodeNum,dim))
+    for i in range(animateIteration):
+        nodePosAnimateData[i] = nodePosHist[i*frameSampleRate]
+
+    print("Creating Animation")
+    animator = tensegrity_animator(tensegrity)
+    animator.plot_wall(pWall,'x')
+    animator.animate_tensegrity(nodePosHist,True,False)

tensegrity/tensegrity_analysis.py
@@ -125,10 +125,22 @@ class tensegrity_analysis():
                     nodeMaxStress[nodeID] += np.abs(jointStress[jointID])
         return nodeMaxStress
 
+    def compute_wall_collision_force(self, nodePos, nWall:Vec3, kWall, pWall:Vec3):
+        # Compute the stress of each link
+        nodeF = np.zeros(self.nodeNum)
+        nWall = nWall/nWall.norm2() #normalize the direction vector
+        for i in range(self.nodeNum):
+            d=(Vec3(nodePos[i])-pWall).dot(nWall)
+            if d<0:
+                normalForce = -kWall*d*nWall
+                nodeF[i] = normalForce.norm2()
+        return nodeF
+
+
     def compute_min_prop_dist_to_face(self, nodePos, propOffset = 0):
         """
         Warning: This tools involves geometric assumption that may no longer hold when large deformation takes place! 
-        Please use this function with caution. 
+        Please use this function with caution!
 
         Input: nodePos - position of tensegrity nodes and quadcopter nodes
                propOffset - vertical offset distance of propeller (i.e)
@@ -140,8 +152,6 @@ class tensegrity_analysis():
 
         Output: shortest distance to surface for each propeller. Notice that this distance should be smaller than propR to prevent exposure.         
         """
-
-
         # Compute the distance from propeller nodes to tensegrity surface
 
         quadNode = nodePos[12:,:]

tensegrity/tensegrity_ode.py
@@ -217,17 +217,7 @@ class tensegrity_ode():
             return func
         return decorator
 
-    @eventAttr()
-    def wall_check_simple(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
-        """
-        Check if the structure has stopped touching the wall. 
-        To decrease the computation. We assume: 1) normal direction of the wall is +x direction
-                                                2) the wall surface is at the plane that x=0
-        """
-        nodes = P[:self.nodeNum*self.dim].reshape((self.nodeNum,self.dim))
-        dMin = np.min(nodes[:,0])        
-        return dMin-self.rL/20.0 #ends when the closest point to wall is 1/20 rL from the surface
-    
+
     @eventAttr()
     def wall_check(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
         """
@@ -241,3 +231,25 @@ class tensegrity_ode():
             if d>dMin:
                 dMin = d
         return dMin
+
+    @eventAttr()
+    def wall_check_simple(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
+        """
+        Check if the structure has stopped touching the wall. 
+        To decrease the computation. We assume: 1) normal direction of the wall is +x direction
+                                                2) the wall surface is at the plane that x=0
+        """
+        nodes = P[:self.nodeNum*self.dim].reshape((self.nodeNum,self.dim))
+        dMin = np.min(nodes[:,0])        
+        return dMin-self.rL/20.0 #ends when the closest point to wall is 1/20 rL from the surface
+    
+    @eventAttr()
+    def vel_check_simple(self, t, P, nWall:Vec3, kWall, pWall:Vec3):
+        """
+        Stop the simulation when COM velocity is pointing away from the wall with a certain threshold speed.
+        We expect maximum deformation to take place before this moment.  
+        """
+        threshold = 0.1 #[m/s]
+        vels = P[self.nodeNum*self.dim:].reshape((self.nodeNum,self.dim)) 
+        vel_x = vels[:,0]
+        return (vel_x-threshold).dot(self.massList) #weighted average of node velocity